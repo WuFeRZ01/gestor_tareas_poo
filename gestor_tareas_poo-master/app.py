04aa3a0413b0748336c5e3497478803bdd21955e
ocamilavillero09 <111584990+ocamilavillero09@users.noreply.github.com>
2024-10-15 11:12:54 -0500

    Delete gestor_tareas_poo-master directory

--- gestor_tareas_poo-master/app.py
+++
@@ -1,6 +0,0 @@
-from gestordetareas.view.console import UIConsola
-from gestordetareas.view.console import Gestortareas
-if __name__ == "_main_":
-    Gestortareas = Gestortareas()
-    consola = UIConsola(Gestortareas)
-    consola.ejecutar_app()
